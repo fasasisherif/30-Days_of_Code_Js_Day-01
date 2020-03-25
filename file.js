4ff6200d627b4724c40c62f376b5893517e01f19
fasasisherif <59504098+fasasisherif@users.noreply.github.com>
2020-03-25 21:49:13 +0100

    Add files via upload

---
+++ file.js
@@ -0,0 +1,19 @@
+var number_1 = 2
+var number_2 = -3
+
+
+if (number_1>0){
+	console.log("number_1 is a positive number")
+}
+else{
+	console.log("number_1 is a negative number")
+}
+
+if (number_2>0){
+	console.log("number_2 is a positive number")
+}
+else{
+	console.log("number_2 is a negative number")
+}
+
+
